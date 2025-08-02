1c4f8ef4d55e294ace0e20c486ef7bfa419d347d
shrutiii <shrutijadhav4102@gmail.com>
2025-08-02 16:27:35 +0530

    configured config.ts

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -1,10 +1,30 @@
 export default{
     port: process.env.PORT || 3000,
     allowed_origins: process.env.ALLOWED_ORIGINS || "*",
-    JWT_SECRET: process.env.JWT_SECRET!,
-    SALTING: process.env.SALTING!,
+
+    JWT_SECRET: ()=>{
+        if (!process.env.JWT_SECRET) {
+            throw new Error("JWT_SECRET is not defined in environment variables");
+        }
+        return process.env.JWT_SECRET;
+    },
+    SALTING: ()=>{
+        if (!process.env.SALTING) {
+            throw new Error("SALTING is not defined in environment variables");
+        }
+        return process.env.SALTING;
+    },
+    API_URL:()=>{
+        if (!process.env.API_URL) {
+            throw new Error("API_URL is not defined in environment variables");
+        }
+        return process.env.API_URL;
+    },
+
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID!,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET!,
     GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL!,
-    API_URL: process.env.API_URL!
+    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID!,
+    GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET!,
+    GITHUB_CALLBACK_URL: process.env.GITHUB_CALLBACK_URL!
 }
