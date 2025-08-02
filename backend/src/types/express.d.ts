bbb7da43b74740a339ec60178aa20c3121cce66f
shrutiii <shrutijadhav4102@gmail.com>
2025-08-03 02:24:57 +0530

    Updated updation schema

--- backend/src/types/express.d.ts
+++ backend/src/types/express.d.ts
@@ -4,7 +4,6 @@ import {MulterFile} from 'multer';
 declare global {
     namespace Express {
         interface Request {
-            adminId?: string;
             userId?: string;
             file?: MulterFile;
         }
