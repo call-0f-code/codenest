d4ce2da5fd9bf4ff6754322796739cfdc1f6f3d1
shrutiiiyet <167532965+shrutiiiyet@users.noreply.github.com>
2025-10-15 21:54:18 +0000

    Clean up memberApi.js by removing commented routes
    Removed commented-out routes for member API.

--- frontend/src/utils/api/memberApi.js
+++ frontend/src/utils/api/memberApi.js
@@ -1,19 +1,5 @@
 import api from './api';
 
-//  router.post('/signup', validate(CreateUserSchema), memberCtrl.createMember);
-//     router.post('/signin', validate(SigninSchema), memberCtrl.login);
-//     router.post('/forgotPassword',validate(forgotPasswordSchema), memberCtrl.forgotpassword);
-//     router.post('/verifyOtp',memberCtrl.verifyOtp)
-
-//     router.use(auth);
-
-//     router.post('/resetPassword', memberCtrl.resetpassword);
-//     router.patch('/:memberId', memberCtrl.updateMember);
-//     router.get('/:memberId', memberCtrl.getDetails);
-//     router.get('/:memberId/achievements', memberCtrl.getAchievements);
-//     router.get('/:memberId/projects', memberCtrl.getProjects);
-//    router.get('/:memberId/interviews', memberCtrl.getInterviews);
-
 export const signIn = async(email, password) => {
     const response = await api.post('/members/signin', {email: email, password: password});
     return response.data;
