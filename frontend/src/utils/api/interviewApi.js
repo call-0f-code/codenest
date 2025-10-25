1e6929a4849d221ca95bd5cb04d2f91b8e5f6736
Sarvesh Shahane <sarveshshahane2004@gmail.com>
2025-10-25 17:03:15 +0530

    add interviewApi in the frontend

---
+++ frontend/src/utils/api/interviewApi.js
@@ -0,0 +1,30 @@
+import api from "./api";
+
+const headers = {
+    "Content-Type" : "multipart/form-data"
+}
+
+export const getAllInterviews = async () => {
+    const response = await api.get("/interviews");
+    return response.data;
+}
+
+export const getInterviewById = async (interviewId) => {
+    const response = await api.get(`/interviews/${interviewId}`);
+    return response.data;
+}
+
+export const createInterview = async (formData) => {
+    const response = await api.post(`/interviews/`, formData, { headers });
+    return response.data;
+}
+
+export const updateInterview = async (interviewId, formData) => {
+    const response = await api.patch(`/interviews/${interviewId}`, formData, { headers });
+    return response.data;
+    
+}
+export const deleteInterview = async (interviewId) => {
+    const response = await api.delete(`/interviews/${interviewId}`);
+    return response.data
+}
