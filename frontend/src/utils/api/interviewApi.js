1c37309105755612944c4c5b8b63f6a18c549061
Sarvesh Shahane <sarveshshahane2004@gmail.com>
2025-10-27 02:21:23 +0530

    refactor function expression names to be more accurate

--- frontend/src/utils/api/interviewApi.js
+++ frontend/src/utils/api/interviewApi.js
@@ -4,27 +4,27 @@ const headers = {
     "Content-Type" : "multipart/form-data"
 }
 
-export const getAllInterviews = async () => {
+export const getAllInterviewExps = async () => {
     const response = await api.get("/interviews");
     return response.data;
 }
 
-export const getInterviewById = async (interviewId) => {
+export const getInterviewExpById = async (interviewId) => {
     const response = await api.get(`/interviews/${interviewId}`);
     return response.data;
 }
 
-export const createInterview = async (formData) => {
+export const createInterviewExp = async (formData) => {
     const response = await api.post(`/interviews/`, formData, { headers });
     return response.data;
 }
 
-export const updateInterview = async (interviewId, formData) => {
+export const updateInterviewExp = async (interviewId, formData) => {
     const response = await api.patch(`/interviews/${interviewId}`, formData, { headers });
     return response.data;
     
 }
-export const deleteInterview = async (interviewId) => {
+export const deleteInterviewExp = async (interviewId) => {
     const response = await api.delete(`/interviews/${interviewId}`);
     return response.data
 }
