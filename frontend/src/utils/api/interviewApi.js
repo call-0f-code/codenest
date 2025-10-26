10ae61fad2f5afbb45ef8ecfbed97d57e89cbcdf
Sarvesh Shahane <sarveshshahane2004@gmail.com>
2025-10-27 03:29:26 +0530

    remove form-data header since the browser sets it

--- frontend/src/utils/api/interviewApi.js
+++ frontend/src/utils/api/interviewApi.js
@@ -1,30 +1,26 @@
 import api from "./api";
 
-const headers = {
-    "Content-Type" : "multipart/form-data"
-}
-
 export const getAllInterviewExps = async () => {
-    const response = await api.get("/interviews");
-    return response.data;
-}
+  const response = await api.get("/interviews");
+  return response.data;
+};
 
 export const getInterviewExpById = async (interviewId) => {
-    const response = await api.get(`/interviews/${interviewId}`);
-    return response.data;
-}
+  const response = await api.get(`/interviews/${interviewId}`);
+  return response.data;
+};
 
 export const createInterviewExp = async (formData) => {
-    const response = await api.post(`/interviews/`, formData, { headers });
-    return response.data;
-}
+  const response = await api.post(`/interviews/`, formData);
+  return response.data;
+};
 
 export const updateInterviewExp = async (interviewId, formData) => {
-    const response = await api.patch(`/interviews/${interviewId}`, formData, { headers });
-    return response.data;
-    
-}
+  const response = await api.patch(`/interviews/${interviewId}`, formData);
+  return response.data;
+};
+
 export const deleteInterviewExp = async (interviewId) => {
-    const response = await api.delete(`/interviews/${interviewId}`);
-    return response.data
-}
+  const response = await api.delete(`/interviews/${interviewId}`);
+  return response.data;
+};
