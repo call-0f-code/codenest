139239a02de0d9f8b8c3fba941c8fb5e792d7e0b
Sarvesh Shahane <sarveshshahane2004@gmail.com>
2025-10-27 23:57:40 +0530

    remove formdata...the controller didn't expect it for some reason

--- frontend/src/utils/api/interviewApi.js
+++ frontend/src/utils/api/interviewApi.js
@@ -1,9 +1,5 @@
 import api from "./api";
 
-const headers = {
-    "Content-Type" : "multipart/form-data"
-}
-
 export const getAllInterviewExps = async () => {
   const response = await api.get("/interviews");
   return response.data;
@@ -14,13 +10,13 @@ export const getInterviewExpById = async (interviewId) => {
   return response.data;
 };
 
-export const createInterviewExp = async (formData) => {
-  const response = await api.post(`/interviews/`, formData, { headers });
+export const createInterviewExp = async (interviewData) => {
+  const response = await api.post(`/interviews/`, interviewData);
   return response.data;
 };
 
-export const updateInterviewExp = async (interviewId, formData) => {
-  const response = await api.patch(`/interviews/${interviewId}`, formData, { headers });
+export const updateInterviewExp = async (interviewId, interviewData) => {
+  const response = await api.patch(`/interviews/${interviewId}`, interviewData);
   return response.data;
 };
 
