9492450ff28f516a231f62315b63618b6e202075
Sarvesh Shahane <sarveshshahane2004@gmail.com>
2025-11-02 17:55:01 +0530

    fix interviewApi

--- frontend/src/utils/api/interviewApi.js
+++ frontend/src/utils/api/interviewApi.js
@@ -7,20 +7,20 @@ export const getAllInterviewExps = async () => {
 
 export const getInterviewExpById = async (interviewId) => {
   const response = await api.get(`/interviews/${interviewId}`);
-  return response.data;
+  return response.data.data;
 };
 
 export const createInterviewExp = async (interviewData) => {
   const response = await api.post(`/interviews/`, interviewData);
-  return response.data;
+  return response.data.data;
 };
 
 export const updateInterviewExp = async (interviewId, interviewData) => {
   const response = await api.patch(`/interviews/${interviewId}`, interviewData);
-  return response.data;
+  return response.data.data;
 };
 
 export const deleteInterviewExp = async (interviewId) => {
   const response = await api.delete(`/interviews/${interviewId}`);
-  return response.data;
+  return response.data.data;
 };
